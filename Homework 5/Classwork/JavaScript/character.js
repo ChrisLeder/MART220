3c53255825a653d11cd558af3ccd99e0f2ac2373
Chris Leder <156860779+ChrisLeder@users.noreply.github.com>
2025-02-23 14:48:23 -0700

    CH-CH-Changes.

---
+++ Homework 5/Classwork/JavaScript/character.js
@@ -0,0 +1,23 @@
+//this is my character class
+class character {
+    constructor(path, x, y) {
+        this.path = path;
+        this.myImage = loadImage(this.path);
+        this.x = x;
+        this.y = y;
+        this.imageWidth = 75;
+        this.imageHeight = 100;
+    }
+    draw() {
+        this.imageHeight(this.myImage, this.x, this.y, 150, 200);
+    }
+    hasCollided(x2, y2, w2, h2) {
+        return(
+            this.x < x2 + w2 &&
+            this.x + this.imageWidth > x2 &&
+            this.y < y2 + h2 &&
+            this.y + this.imageHeight > y2
+              );
+
+    }
+}
---
+++ Homework 5/Classwork/JavaScript/sketch.js
@@ -0,0 +1,68 @@
+var animation = [];
+var runAnimation = [];
+var i = 0;
+var myCharacter;
+var myFood;
+var x = 100;
+var y = 100;
+var foodArray = [];
+var foodFound = false;
+
+function preload()
+{
+for (var i = 0; i <10; i++)
+{   //concatenation-adding strings together
+    myCharacter = new character(x,y, "images/character/Run_00" + i + ".png", x, y);
+    animation.push(myCharacter);  
+}
+
+}
+function setup()
+{
+    createCanvas(800,800);
+    setInterval(updateIndex,30);
+    for (let i =0; i <5; i++){
+        myFood = new food(random(100,600), random(100, 600), 50);
+        foodArray.push(myFood);
+    }
+}
+function draw()
+{
+    background(120);
+    animation[i].draw();
+    for (let i = 0; i< foodArray.length; i++) {
+        foodArray[i].draw(); 
+    }
+    
+if (keyIsPressed){
+    if (key == "a") {
+        x--;
+    }
+    if (key == "d") {
+        x++;
+    }
+    if (key == "w") {
+        y--;
+    }
+    if (key == "s") {
+        y++;
+    }
+    for (let i =0; i < 10; i++){
+animation[i].x = 150;
+animation[i].y = 200;
+    }
+
+    for(let k = 0; k < foodArray.length; k++){
+        if (animation[i].hasCollided(foodArray[k].x, foodArray[k].y, 25, 25)) { 
+            foodArray.splice(k,1);
+    }
+}
+        
+}
+
+function updateIndex()
+{
+    i++;
+    if (i>9)
+        {i = 0;}
+}
