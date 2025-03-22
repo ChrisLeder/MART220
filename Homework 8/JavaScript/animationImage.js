e9702d402e2a1fc444d3f93c8c616d1d3124ba07
Chris Leder <156860779+ChrisLeder@users.noreply.github.com>
2025-03-22 13:39:08 -0600

    animation data

--- Homework 8/JavaScript/animationImage.js
+++ Homework 8/JavaScript/animationImage.js
@@ -3,11 +3,11 @@ class animationImage
 
     constructor(fileNames, x, y, w, h)
     {
-       //this.fileNames =this.fileNames;
+        this.fileNames =this.fileNames;
         this.x = x;
         this.y = y;
-        this.w = w;
-        this.h = h;
+        this.w = 50;
+        this.h = 75;
         this.imageObjcts = [];
         //this.loadAnimation();
         this.currentAnimation;
@@ -25,7 +25,7 @@ class animationImage
 
     setX(x)
     {
-        this.x =x;
+        this.x = x;
     }
 
     setCurrentFrameCount(currentFrameCount)
@@ -35,32 +35,36 @@ class animationImage
 
     createAnimation()
     {
-        this.currentAnimation = createSprite(300, 250);
+        this.currentAnimation = createSprite(50, 75,50 ,75);
     }
 
     loadAnimation(animationType, fileNames)
     {
        this.currentAnimation.addAnimation(animationType, fileNames[0], fileNames[fileNames.length-1]); 
     }
+    updatePosition(direction)
+    {
+        this.direction = direction;
+    }
 
     drawAnimation(animationType)
     {
         this.currentAnimation.frameDelay = 5;
-        this.currentAnimation.scale = 5;
+        this.currentAnimation.scale = 0.25;
         this.currentAnimation.changeAnimation(animationType);
 
         if (animationType =='run' && this.direction == 'forward')
             {
                 this.currentAnimation.mirror.x = false;
                 this.currentAnimation.direction = 0;
-                this.currentAnimation.speed = 3;
+                this.currentAnimation.velocity.x = 3;
 
             }
         else if (animationType == 'run'&& this.direction == 'reverse')
             {
                 this.currentAnimation.mirror.x = true;
                 this.currentAnimation.direction = 180;
-                this.currentAnimation.speed = 3;
+                this.currentAnimation.velocity.x = 3;
             }
         else
         {
--- Homework 8/JavaScript/sketch.js
+++ Homework 8/JavaScript/sketch.js
@@ -5,31 +5,141 @@ var runPaths = [];
 var myAnimation;
 var myRunAnimation;
 //var someImage;
-
+var myFood;
+var x = 100;
+var y = 100;
+var foodArray = [];
+var score = 5;
+var timeRemaining = 0;
+var counter = 0;
+var timeLeft = 30;
+var timeremaining = 0
+var bgmusic;
 
 function preload()
 {
-    idlePaths = loadStrings("../images/NinjaGirl/idle.txt");
-    runPaths = loadStrings("../images/NinjaGirl/run.txt");
+    //sound
+    bgmusic = loadSound("Audio/bgMusic.wav");
+    eat = loadSound("Audio/yum.wav");
+    retch = loadSound("Audio/retching.wav");
+    //Character animation
+    idlePaths = loadStrings("../Assets/images/NinjaGirl/idle.txt");
+    runPaths = loadStrings("../Assets/images/NinjaGirl/run.txt");
 }
 
 function setup()
 {
     createCanvas(800, 650);
-    myAnimation =new animationImage(idlePaths, 0, 0, 150, 150);
+
+    setInterval(foodFight, 5000);
+    
+    var interval = setInterval(countDown, 1000);
+
+    function countDown() {
+        counter++;
+        timeLeft--;
+        if  (timeLeft== 0) {
+            clearInterval(interval);
+        }
+    }    
+
+    setInterval(updateIndex, 50);
+    for (let i = 0; i < 5; i++) {
+        myFood = new food(random(100, 600), random(100, 600),  34, 86, 214, 50);
+        foodArray.push(myFood);}
+
+        for (let i = 0; i < 5; i++) {
+            myFood = new food(random(100, 600), random(100, 600),  100, 140, 43, 50);
+            foodArray.push(myFood);
+    }
+
+
+
+    myAnimation =new animationImage(idlePaths, 0, 0, 50, 75);
     myAnimation.loadAnimation('idle', idlePaths);
     myAnimation.loadAnimation('run', runPaths);
 
     //compact way to add someImage
-    /*someeImage =createSprite(450, 200, 100, 100, 'static');
+    /*someImage =createSprite(450, 200, 100, 100, 'static');
     someImage.img = "./images/some.jpg";
     someImage.scale = 0.05;
     someImage.diameter = 150;*/
+
+    bgSound();//this is in setup function
 }
+
+function loadFood()
+    {
+for (let i = 0; i < foodArray.length; i++) 
+        {
+        foodArray[i].draw();
+        }
+    }
+    function foodFight(){
+        for (let i = 0; i < foodArray.length; i++) 
+            {
+            foodArray[i].x = random(100, 600);
+            foodArray[i].y= random(100, 600);
+            }
+            
+    }
+    
+
+    function bgSound(){
+        bgmusic.play();
+        bgmusic.loop();
+        bgmusic.setVolume(0.1);
+        userStartAudio();
+    }
 
 function draw()
 {
     background(13, 152, 34);
+    loadFood();
+
+     //Score
+     fill(214, 35, 20);
+     stroke(3);
+     rect(35, 30, 50, 30);
+     fill(214, 35, 20);
+     stroke(3);
+     textSize(24);
+     text('Health', 25, 25);
+     fill(0);
+     text(score, 48, 55);
+ 
+     //timer
+     fill(214, 35, 20);
+     stroke(3);
+     textSize(24);
+     text('Time Remaining', 610, 25);
+     fill(214, 35, 20);
+     stroke(3);
+     rect(680, 30, 70, 30);
+     fill(0);
+     text(timeLeft, 705, 55);
+     //Game Text
+     if (timeLeft== 0) {
+         stroke(15);
+         fill(214, 35, 20);        
+         textSize(60);
+         text('Game Over', 225, 60);
+     }    
+     if (score == 0){
+         stroke(15);
+         fill(214, 35, 20);        
+         textSize(80);
+         text('You Died!!!', 225, 400); 
+         textSize(60);
+         text('Game Over', 225, 60);
+     }
+     if (score == 10){
+         stroke(15);
+         fill(214, 35, 20);        
+         textSize(80);
+         text('You Win!!!', 225, 400); 
+         textSize(60);
+         text('Game Over', 225, 60);}
 
     if(kb.pressing('d'))
     {
@@ -59,8 +169,47 @@ function draw()
         myAnimation.drawAnimation('idle');
     }
 
-   /* myAnimation.updatePosition('idle');
-    myAnimation.stCurrentFramCount(frameeCount);
+    {
+        for (let k = 0; k < foodArray.length; k++) 
+            {
+            //if (animation[i].hasCollided(foodArray[k].x, foodArray[k].y, 10, 10)) 
+            if(collideRectCircle(animation[i].x, animation[i].y, animation[i].imageWidth, animation[i].imageHeight, foodArray[k].x, foodArray[k].y, 10, 10 ))
+                {
+                    if (foodArray[k].r==34){
+                        eat.play();
+                        score = score + 1;
+                    }
+                    else{
+                        retch.play();
+                        score= score - 1;
+
+                    }
+
+                foodArray.splice(k, 1);               
+                
+                }
+            } else 
+            {
+                animation[i].draw();
+            } 
+            if ( timeLeft== 0 || score == 0){
+                keyIsPressed = false;
+            }
+            if (score == 10){
+                keyIsPressed = false;
+            }
+            
+        }
+        
+        function updateIndex() 
+        {
+            i += 1;
+            if (i > result.length - 1
+            ) { i = 0; }
+        }
+        
+    /*myAnimation.updatePosition('idle');
+    myAnimation.stCurrentFramCount(frameCount);
     myAnimation.drawAnimation();*/
 
 
