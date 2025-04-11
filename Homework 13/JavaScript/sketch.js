906e6e8af225e8dd26f53f34b5d3ff0fb2882294
Chris Leder <156860779+ChrisLeder@users.noreply.github.com>
2025-04-11 17:16:23 -0600

    started

---
+++ Homework 13/JavaScript/sketch.js
@@ -0,0 +1,20 @@
+//This is my Creative Coding II Homework 13 on 3D Modeling
+
+function preload()
+{
+    img1 = loadImage("../images/image1.jpg");
+    img2 = loadImage("../images/image2.jpg");
+    img3 = loadImage("../images/image3.jpg");
+    img4 = loadImage("../images/image4.jpg");
+    img5 = loadImage("../images/image5.jpg");
+}
+function setup()
+{
+createCanvas(windowWidth, windowHeight, WEBGL);
+
+
+}
+function draw()
+{
+
+}
---
+++ Homework 13/README.md
@@ -0,0 +1,3 @@
+This is my Creative Coding II Homework 13 assignment on 3D Objects
+
+
