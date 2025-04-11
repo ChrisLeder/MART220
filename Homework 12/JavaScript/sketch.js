d017568410617bf779f495c67624ef4635bb6e18
Chris Leder <156860779+ChrisLeder@users.noreply.github.com>
2025-04-11 16:08:19 -0600

    Done?

--- Homework 12/JavaScript/sketch.js
+++ Homework 12/JavaScript/sketch.js
@@ -1,4 +1,7 @@
+// This is my Sketch JS for HW 12
 
+var baseImage;
+var texturesCanvas;
 
 
 function preload()
@@ -8,12 +11,21 @@ img2 = loadImage("../images/image2.jpg");
 img3 = loadImage("../images/image3.jpg");
 img4 = loadImage("../images/image4.jpg");
 img5 = loadImage("../images/image5.jpg");
+baseImage = loadImage("../images/image5.jpg");
 
 
     myFont = loadFont('Fonts/Rye-Regular.ttf');
 }
 function setup() {
     createCanvas(windowWidth, windowHeight, WEBGL);
+
+    texturedCanvas = createGraphics(512, 256);
+    texturedCanvas.image(baseImage, 0, 0, texturedCanvas.width, texturedCanvas.height);
+    texturedCanvas.fill(126, 12, 112);
+    texturedCanvas.textAlign(CENTER, CENTER);
+    texturedCanvas.textSize(50);
+    texturedCanvas.textFont(myFont);
+    texturedCanvas.text("And So It Begins...", texturedCanvas.width / 2, texturedCanvas.height / 2);
 
     
     
@@ -41,13 +53,9 @@ function draw() {
     {               
         push();
         translate(-400,-150);
-        rotateY(frameCount * 0.01); 
-        /*textFont(myFont);
-    textSize(50);
-    fill('172, 160, 12');
-    text("And So It Begins", -width/20, -height/30, 0);*/      
-        texture(img5);
-        cylinder(200, 150, 24, 24, true, true);
+        rotateY(frameCount * -0.01);             
+        texture(texturedCanvas);
+        cylinder(200, 150, 48, 1, true, true);
         pop();
         }
         //torus
